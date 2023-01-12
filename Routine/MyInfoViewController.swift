8db153ab7c7e8f167e04c51b550afd00c3e32cd3
dohyun <thtree523@gmail.com>
2023-01-12 18:27:00 +0900

    feat 할일 목록 UI 구성 Swift Packge manger를 사용하여 SnapKit 설치 텝바와 네비게이션으로 구성되어 있다보니 Xib 파일로 작성하기 어려웠음(텝바와, 네비게이션이 Xib로는 보이지 않음) 또한 인터페이스 빌더를 사용하는데에도 배우는 시간적 소모가 크다고 생각하여 SnapKit을 사용하여 UI 구성 issue#4

---
+++ Routine/MyInfoViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MyInfoViewController.swift
+//  Routine
+//
+//  Created by 김도현 on 2023/01/10.
+//
+
+import UIKit
+
+class MyInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Routine/TabBarController.swift
@@ -0,0 +1,44 @@
+//
+//  MainViewController.swift
+//  Routine
+//
+//  Created by 김도현 on 2023/01/10.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarSetting()
+        viewControllerSetting()
+    }
+    
+    private func tabBarSetting() {
+        self.tabBar.backgroundColor = .white
+        self.modalPresentationStyle = .fullScreen
+        self.tabBar.unselectedItemTintColor = .systemGray
+        self.tabBar.tintColor = UIColor.mainColor
+    }
+    
+    private func viewControllerSetting() {
+        let vc1 = UINavigationController(rootViewController: ListViewController())
+        let vc2 = UINavigationController(rootViewController: MyInfoViewController())
+        let vc3 = UINavigationController(rootViewController: SettingViewController())
+
+        vc1.title = "목록"
+        vc2.tabBarItem.title = "나의 루틴"
+        vc3.title = "설정"
+
+        self.setViewControllers([vc1, vc2, vc3], animated: false)
+        
+        guard let items = self.tabBar.items else { return }
+        
+        let images = ["list.bullet", "person.fill", "gearshape.fill"]
+        
+        for index in 0..<items.count {
+            items[index].image = UIImage(systemName: images[index])
+        }
+    }
+}
